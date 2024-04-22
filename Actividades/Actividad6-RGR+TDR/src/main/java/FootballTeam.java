b0674fc7ef590f891366d88e26af82681ce42f45
Juan Silva Rojas <juan.silva.r@uni.pe>
2024-04-21 22:07:18 -0500

    Actividad 6 completada

--- Actividades/Actividad6-RGR+TDR/src/main/java/FootballTeam.java
+++ Actividades/Actividad6-RGR+TDR/src/main/java/FootballTeam.java
@@ -1,8 +1,34 @@
-public class FootballTeam {
+public class FootballTeam implements Comparable<FootballTeam> {
+
+    private int gamesWon;
+
     public FootballTeam(int gamesWon) {
+        if (gamesWon < 0) {
+            throw new IllegalArgumentException
+                    ("Not possible to have less than 0 games won! (was + " + gamesWon + ")");
+        }
+
+        this.gamesWon = gamesWon;
     }
+
     public int getGamesWon() {
-        //return 0;
-        return 3;
+        return gamesWon;
+    }
+
+//    @Override
+//    public int compareTo(FootballTeam otherTeam) {
+//        if (gamesWon > otherTeam.getGamesWon()) {
+//            return 1;
+//        }
+//        else if (gamesWon < otherTeam.getGamesWon()) {
+//            return -1;
+//        }
+////      return 23948732;
+//        return 0;
+//    }
+
+    @Override
+    public int compareTo(FootballTeam otherTeam) {
+        return gamesWon - otherTeam.getGamesWon();
     }
 }
---
+++ Actividades/Actividad6-RGR+TDR/src/main/java/lang/Comparable.java
@@ -0,0 +1,5 @@
+package lang;
+
+public interface Comparable<FootballTeam> {
+    public int compareTo(FootballTeam otherTeam);
+}
