5406b3f1b6a02684b26a038d63f3d2937aef7988
Juan Silva Rojas <juan.silva.r@uni.pe>
2024-04-21 19:10:34 -0500

    Actividad 5 completada

--- Actividades/Actividad5-TDD/TDD-3/src/main/java/com/wordz/domain/Score.java
+++ Actividades/Actividad5-TDD/TDD-3/src/main/java/com/wordz/domain/Score.java
@@ -6,81 +6,39 @@ import java.util.List;
 
 public class Score {
     private final String correct;
-    private Letter resultado = Letter.INCORRECT;
-    private int position; //(**)
+    private final List<Letter> results = new ArrayList<>();
+    private int position;
 
     public Score(String correct) {
         this.correct = correct;
     }
 
     public Letter letter(int position) {
-        return resultado;
+        return results.get(position);
     }
 
-    //Metodo assess No Refactorizado
-    //Aqui se verifica si la letra de entrada coincide en la posicion correcta con la lerta de la palabra
-//    public void assess(int position, String attempt) {
-//        if (correct.charAt(position) == attempt.charAt(position)) {
-//            resultado = Letter.CORRECT;
-//        }
-//    }
-
-//----------------------------------------------------------------------------------------
-    //Metodo assess refactorizado: (*)
-//    public void assess(int position, String attempt){
-//        if(isCorrectLetter(position,attempt)){
-//            resultado = Letter.CORRECT;
-//        }
-//    }
-
-    //(*)
-//    private boolean isCorrectLetter(int position, String attempt){
-//        return correct.charAt((position)) == attempt.charAt(position);
-//    }
-
-//----------------------------------------------------------------------------------------
-    //Segunda Refacotirzacion de assess (cambio de diseño, un parametro en isCorrectLetter) (**)
-//    public void assess(String attempt){
-//        if(isCorrectLetter(attempt)){
-//            resultado = Letter.CORRECT;
-//        }
-//    }
-//
-//    private boolean isCorrectLetter(String attempt){
-//        return correct.charAt(position) == attempt.charAt(position);
-//    }
-//----------------------------------------------------------------------------------------
-    //Refactorizacion para que funcione con segunda letra
-//    public void assess(String attempt) {
-//        for (char current: attempt.toCharArray()) {
-//            if (isCorrectLetter(current)) {
-//                resultado = Letter.CORRECT;
-//            } else if (occursInWord(current)) {
-//                resultado = Letter.PART_CORRECT;
-//            }
-//        }
-//    }
-
-    //En el pdf añaden atributo position, pero eso rompe
-    // todo, aqui esta la solucion
     public void assess(String attempt) {
-        for (int i = 0; i < attempt.length(); i++) {
-            char current = attempt.charAt(i);
-            this.position = i; // Ajusta position para cada caracter evaluado
-            if (isCorrectLetter(current)) {
-                resultado = Letter.CORRECT;
-            } else if (occursInWord(current)) {
-                resultado = Letter.PART_CORRECT;
-            }
+        for (char current: attempt.toCharArray()) {
+            results.add( scoreFor(current) ); //Separamos las condicionales aparte en una funcion
+            position++;
         }
+    }
+
+    private Letter scoreFor(char current) {
+        if (isCorrectLetter(current)) {
+            return Letter.CORRECT;
+        }
+        if (occursInWord(current)) {
+            return Letter.PART_CORRECT;
+        }
+        return Letter.INCORRECT;
+    }
+
+    private boolean occursInWord(char current) {
+        return correct.contains(String.valueOf(current));
     }
 
     private boolean isCorrectLetter(char currentLetter) {
         return correct.charAt(position) == currentLetter;
-    }
-
-    //Verifica si la letra actual esta en cualquier parte de la palabra
-    private boolean occursInWord(char current) {
-        return correct.contains(String.valueOf(current));
     }
 }
--- Actividades/Actividad5-TDD/TDD-3/src/test/java/com/wordz/domain/WordTest.java
+++ Actividades/Actividad5-TDD/TDD-3/src/test/java/com/wordz/domain/WordTest.java
@@ -6,35 +6,41 @@ import static com.wordz.domain.Letter.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class WordTest {
+    @Test
+    public void oneIncorrectLetter() {
+        var word = new Word("A");
+        var score = word.guess("Z");
+        assertScoreForGuess(score, INCORRECT);
+    }
 
     @Test
     public void oneCorrectLetter() {
         var word = new Word("A"); // Parece que se crea una palabra por adivinar
         var score = word.guess("A"); // guess parece ser un metodo que recibe un intento y devuelve un resultado que indicará si es correcto o no
-        assertThat(score.letter(0)).isEqualTo(CORRECT); // Se verifica que la avininaza de la letra es correcta
-
-        //Tomamos el assert anterior y lo movemos a un metodo privado
-        assertScoreForLetter(score,0, CORRECT);
+        assertScoreForGuess(score, CORRECT); // Se verifica que la avininaza de la letra es correcta
     }
 
     @Test
-    void allScoreCombinations() {
-        var word = new Word("ARI");
-        var score = word.guess("ZAI");
-        assertScoreForLetter(score, 0, Letter.INCORRECT);
-        assertScoreForLetter(score, 1, Letter.PART_CORRECT);
-        assertScoreForLetter(score, 2, Letter.CORRECT);
-    }
-
-    @Test
-    void secondLetterWrongPosition() {
+    public void secondLetterWrongPosition() {
         var word = new Word("AR");
         var score = word.guess("ZA");
-        assertScoreForLetter(score, 1, Letter.PART_CORRECT);
+        assertScoreForGuess(score, INCORRECT, PART_CORRECT);
     }
 
-    private  void assertScoreForLetter(Score score, int position, Letter expected){
-        assertThat(score.letter(position)).isEqualTo(expected);
+    @Test
+    public void allScoreCombinations() {
+        var word = new Word("ARI");
+        var score = word.guess("ZAI");
+        assertScoreForGuess(score, INCORRECT, PART_CORRECT, CORRECT);
+    }
+
+    //Nota: el segundo parametro es un Varargs
+    // permite a los métodos aceptar cero o más argumentos del mismo tipo
+    // sin necesidad de definirlos explícitamente o usar un arreglo.
+    private void assertScoreForGuess(Score score, Letter... expectedScores) {
+        for (int position = 0; position < expectedScores.length; position++) {
+            Letter expected = expectedScores[position];
+            assertThat(score.letter(position)).isEqualTo(expected);
+        }
     }
 }
-
